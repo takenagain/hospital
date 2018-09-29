b29345a3ae3fce0541bc0f05d58092ca67811ef1
Francois van Wyk <searchnot4213@gmail.com>
2018-09-29 22:29:14 +0200

    firebase messaging

---
+++ app/src/main/java/com/melusi/hospital/FirebaseMessaging.java
@@ -0,0 +1,74 @@
+package com.melusi.hospital;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class FirebaseMessaging extends FirebaseMessagingService {
+    NotificationManager notificationManager;
+
+    public FirebaseMessaging() {
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("Token", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Setting up Notification channels for android O and above
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+        int notificationId = new Random().nextInt(60000);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "yes123")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)  //a resource for your custom small icon
+                .setContentTitle(remoteMessage.getData().get("title")) //the "title" value you sent in your notification
+                .setContentText(remoteMessage.getData().get("message")) //ditto
+                .setAutoCancel(true)  //dismisses the notification on click
+                .setSound(defaultSoundUri);
+
+        notificationManager.notify(notificationId /* ID of notification */, notificationBuilder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(){
+        CharSequence adminChannelName = "test";
+        String adminChannelDescription = "yes";
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel("yes123", adminChannelName, NotificationManager.IMPORTANCE_LOW);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
+    }
+}
